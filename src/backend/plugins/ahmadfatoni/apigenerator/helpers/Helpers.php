2636d1891075c5b5f8e76a16e222e4f8559dac52
Rafael Sanchez <rafael@booug.com>
2024-10-24 06:06:22 +0000

    Add tools and configurations.

---
+++ src/backend/plugins/ahmadfatoni/apigenerator/helpers/Helpers.php
@@ -0,0 +1,20 @@
+<?php namespace AhmadFatoni\ApiGenerator\Helpers;
+
+Class Helpers {
+
+	public function apiArrayResponseBuilder($statusCode = null, $message = null, $data = [])
+	{
+		$arr = [
+			'status_code' => (isset($statusCode)) ? $statusCode : 500,
+			'message' => (isset($message)) ? $message : 'error'
+		];
+		
+		if ( is_array($data) && count($data) > 0) {
+			$arr['data'] = $data;
+		}
+
+		return response()->json($arr, $arr['status_code']);
+		//return $arr;
+		
+	}
+}
